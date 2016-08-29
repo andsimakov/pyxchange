b0b67944c0cf5bf921cdf55db68a3608aa901691
Andrii Simakov <and.simakov@gmail.com>
2016-08-29 20:12:04 +0300

    Test

--- final/settings.py
+++ final/settings.py
@@ -47,12 +47,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'final.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
