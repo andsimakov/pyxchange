7d390cf88834d65cb2d5da5b735a2ea848663628
Andrii Simakov <and.simakov@gmail.com>
2016-08-30 20:28:55 +0300

    Override settings test

--- final/settings.py
+++ final/settings.py
@@ -48,9 +48,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'final.wsgi.application'
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'nO3E4X1cds',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
