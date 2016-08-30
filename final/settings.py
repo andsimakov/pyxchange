488e98fabd13dabed74efff637a66a2229074031
Andrii Simakov <and.simakov@gmail.com>
2016-08-30 20:17:05 +0300

    Override settings test

--- final/settings.py
+++ final/settings.py
@@ -52,14 +52,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'final',
-    #     'USER': 'pyxchange',
-    #     'PASSWORD': 'nO3E4X1cds',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
-    # }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
